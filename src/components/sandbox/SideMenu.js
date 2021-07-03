4ab4f9521f2c08ab65255dd69cd07733150f2601
kovlento <bluskytee@163.com>
2021-07-03 23:50:13 +0800

    sidemenu

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,28 +1,98 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout, Menu } from 'antd'
 import {
   UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
+  HomeOutlined,
+  LockOutlined,
+  ReadOutlined,
+  HighlightOutlined,
+  SendOutlined,
 } from '@ant-design/icons'
+import { withRouter } from 'react-router-dom'
+import axios from 'axios'
+
+import './index.scss'
 
 const { Sider } = Layout
+const { SubMenu } = Menu
 
-export default function SideMenu() {
+const iconList = {
+  '/home': <HomeOutlined />,
+  '/user-manage': <UserOutlined />,
+  '/user-manage/list': <UserOutlined />,
+  '/right-manage': <LockOutlined />,
+  '/right-manage/role/list': <LockOutlined />,
+  '/right-manage/right/list': <LockOutlined />,
+  '/news-manage': <ReadOutlined />,
+  '/news-manage/add': <ReadOutlined />,
+  '/news-manage/draft': <ReadOutlined />,
+  '/news-manage/category': <ReadOutlined />,
+  '/audit-manage': <HighlightOutlined />,
+  '/audit-manage/audit': <HighlightOutlined />,
+  '/audit-manage/list': <HighlightOutlined />,
+  '/publish-manage': <SendOutlined />,
+  '/publish-manage/unpublished': <SendOutlined />,
+  '/publish-manage/published': <SendOutlined />,
+  '/publish-manage/sunset': <SendOutlined />,
+}
+
+function SideMenu(props) {
+  const [menu, setMenu] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:5000/rights?_embed=children').then((res) => {
+      setMenu(res.data)
+    })
+  }, [])
+
+  const checkPagePermission = (item) => {
+    return item.pagepermisson === 1
+  }
+
+  const renderMenu = (menuList) => {
+    return menuList.map((item) => {
+      if (item.children?.length > 0 && checkPagePermission(item)) {
+        return (
+          <SubMenu key={item.key} icon={iconList[item.key]} title={item.title}>
+            {renderMenu(item.children)}
+          </SubMenu>
+        )
+      }
+      return (
+        checkPagePermission(item) && (
+          <Menu.Item
+            key={item.key}
+            icon={iconList[item.key]}
+            onClick={() => {
+              props.history.push(item.key)
+            }}
+          >
+            {item.title}
+          </Menu.Item>
+        )
+      )
+    })
+  }
+
+  const selectedKey = [props.location.pathname]
+  const openKey = ['/' + props.location.pathname.split('/')[1]]
+
   return (
     <Sider trigger={null} collapsible>
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
-          nav 1
-        </Menu.Item>
-        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-          nav 2
-        </Menu.Item>
-        <Menu.Item key="3" icon={<UploadOutlined />}>
-          nav 3
-        </Menu.Item>
-      </Menu>
+      <div style={{ display: 'flex', height: '100%', flexDirection: 'column' }}>
+        <div className="logo">全球新闻发布管理系统</div>
+        <div style={{ flex: 1, overflow: 'auto' }}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            selectedKeys={selectedKey}
+            defaultOpenKeys={openKey}
+          >
+            {renderMenu(menu)}
+          </Menu>
+        </div>
+      </div>
     </Sider>
   )
 }
+
+export default withRouter(SideMenu)
